fbdba1863bd65ea20140f77abaf985fd7125af83
Neal McConachie <neal@mcconachie.co>
2023-01-27 17:20:41 +0000

    diff-squares: Finish exercise

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,22 @@
 class DifferenceOfSquaresCalculator {
 
-    int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int computeSquareOfSumTo(int n) {
+        // 1 + 2 + 3 + 4
+        // 4 + 3 + 2 + 1
+        // -------------
+        // 5 + 5 + 5 + 5
+        // == (n)(n+1)
+        // == 2x(sum of 1..4)
+        int sum = n * (n + 1) / 2;
+        return sum * sum;
     }
 
-    int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int computeSumOfSquaresTo(int n) {
+        // https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares
+        return n * (n + 1) * (2*n + 1) / 6;
     }
 
-    int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int computeDifferenceOfSquares(int n) {
+        return computeSquareOfSumTo(n) - computeSumOfSquaresTo(n);
     }
-
 }
